a9c6d3bacf4be7efbdf5a13b113e0ab8abe0f9af
ezedin <ezedin.fedlu@gmail.com>
2020-09-13 19:51:44 +0300

    cleaning the repo to start the amazing work

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"github.com/ezedinff/appzoid/routes"
-
-	"github.com/gofiber/fiber"
-	"github.com/gofiber/fiber/middleware"
-)
+import "fmt"
 
 func main() {
-	app := fiber.New()
-	app.Use(middleware.Logger())
-	routes.SetupRoute(app)
-	app.Listen(5000)
+	fmt.Println("Cleaning the work space")
 }
